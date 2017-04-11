8fcf168dd8a2f69359561f1b895d7136bb04be11
Ryan Turner <ryanthepit@gmail.com>
2017-04-10 17:39:39 -0700

    finished first problem

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3,
+  hourlySales:[],
+  getRandomNumCust: function (){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  cookiesPerHour: function(){
+    for(var i = 0; i < 14 ; i++){
+      var avgCookies = this.getRandomNumCust() * this.avgSale;
+      this.hourlySales.push(avgCookies);
+    };
+  }
+};
+firstAndPike.cookiesPerHour();
+console.log(firstAndPike.hourlySales);
+var seaTacAirport = {
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+  getRandomNumCust: function (){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+},
+cookiesPerHour: function(){
+  for(var i = 0; i < 14 ; i++){
+    var avgCookies = this.getRandomNumCust() * this.avgSale;
+    this.hourlySales.push(avgCookies);
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgSale: 2.3,
+  getRandomNumCust: function (){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
+};
+var capitolHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgSale: 2.3,
+  getRandomNumCust: function (){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
+};
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgSale: 4.6,
+  getRandomNumCust: function (){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
+};
